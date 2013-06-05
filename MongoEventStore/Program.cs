1ccc13e3e5c855c6590c86bc8c9d449c3e4e59db
ton.yeung <tyeung@auto-master.com>
2013-06-05 13:33:15 -0500

    Initial Commit

---
+++ MongoEventStore/Program.cs
@@ -0,0 +1,58 @@
+﻿using EventStore;
+using EventStore.Dispatcher;
+using EventStore.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoEventStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var store = Wireup.Init()
+                .UsingMongoPersistence("EventStore", new DocumentObjectSerializer())
+                    .InitializeStorageEngine()
+                    .UsingJsonSerialization()
+                        .Compress()
+                .UsingSynchronousDispatchScheduler()
+                    .DispatchTo(new CommitDispatcher())
+
+                .Build();
+
+            var myMessage = new MyMessage() { CustomerId = Guid.NewGuid(), MessageId = Guid.NewGuid() };
+
+
+            using (store)
+            {
+                using (var stream = store.CreateStream(myMessage.CustomerId))
+                {
+                    stream.Add(new EventMessage { Body = myMessage });
+                    stream.CommitChanges(myMessage.MessageId);
+                }
+            }
+        } 
+    }
+
+    public class MyMessage
+    {
+        public Guid CustomerId { get; set; }
+        public Guid MessageId { get; set; }
+    }
+
+    public sealed class CommitDispatcher : IDispatchCommits
+    {
+        
+        public void Dispatch(Commit commit)
+        {
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    } 
+}
